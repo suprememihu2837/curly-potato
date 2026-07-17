f4383974f0e985c8618d5a3798c6edeece4e2f03
Mihir Anand Sanghavi <shweta.anand.sanghavi@gmail.com>
2026-07-17 23:20:51 +0000

    learned basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountID = 144553
+let accountEmail ="mihirsanghvai28@gmail.com"
+var accountPassword = "12345"
+accountCity ="Aurangabad"
+let accountState;
+
+// accountID =2 //not allowed
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+accountEmail =" hc@hc.com"
+accountPassword="2837"
+accountCity= "Aurangabad"
+
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity,accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict "; //treat all JS code as newer version
+
+//alert(3+3)
+// we are using nodejs, notn browser
+
+console.log(3+3);
+console.log("Mihir");
+
+let name="Mihir"
+let age=" 19"
+let isLoggedIn= false
+let state= null
+
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof null);
+
+
