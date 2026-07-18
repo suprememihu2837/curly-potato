682fa851f550bb2b09e31a0f322565140b23d5aa
Mihir Anand Sanghavi <shweta.anand.sanghavi@gmail.com>
2026-07-18 18:02:06 +0000

    modified converrsion datatype file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,20 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber= 28
 
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// console.log("1"+2);
+// console.log( 1+"2");
+// console.log( "1"+ 2+ 2 );
+// console.log( 1+2+"2");
+
+// console.log(+true)
+// console.log(+"");
+
+let num1, num2 , num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+// console.log(gameCounter)
